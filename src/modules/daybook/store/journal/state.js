34bb5d3a08e39b133c212be15ea1a0a3a83121ef
Jose Antonio <josmen@gmail.com>
2022-02-26 06:32:31 +0100

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Do ex irure laborum sit do duis nisi labore. Cupidatat sunt quis sunt consectetur duis ut ad aliquip commodo nostrud pariatur laboris quis quis. Elit aliquip est irure velit laborum. Aliqua nulla ullamco ea sint ea labore tempor adipisicing.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + Math.floor(Math.random() * 1000),
+            date: new Date().toDateString(),
+            text: 'Velit cillum reprehenderit eiusmod do labore non proident excepteur pariatur aliquip. Dolore incididunt duis aute excepteur cupidatat. Incididunt enim ipsum dolor incididunt eiusmod veniam est deserunt sint do in veniam ad. Ea incididunt dolore irure cupidatat duis exercitation ut ex adipisicing officia nulla. Lorem laborum enim minim dolor duis magna. Reprehenderit veniam elit ullamco reprehenderit voluptate anim officia in eu nisi amet.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + Math.floor(Math.random() * 1000),
+            date: new Date().toDateString(),
+            text: 'Amet labore qui consectetur nulla quis esse qui occaecat aliquip est commodo tempor sint sunt. Qui sit consequat incididunt anim cupidatat cupidatat occaecat labore deserunt ea incididunt irure non pariatur. Deserunt dolore dolore laboris nisi proident id elit exercitation occaecat cillum cillum sint dolore et. Irure consectetur consectetur labore ipsum sunt incididunt cupidatat eiusmod incididunt cillum incididunt elit proident quis. Nostrud reprehenderit aliqua ut esse ipsum aliqua. Ullamco nisi do est ut. Ullamco proident Lorem Lorem sit elit magna in proident id sit.',
+            picture: null,
+        },
+    ]
+})
